94368755eb22927caa93d44dddbcc8a80cd94a94
Sander Dijkhuis <mail@sanderdijkhuis.nl>
2025-02-14 05:57:24 +0100

    feat: automate uploading PMode

--- domibus/configure.sh
+++ domibus/configure.sh
@@ -1,3 +1,8 @@
-# TODO: upload pmodes instead
-curl -v -H "Authorization: Basic cGFkbWluOnBhc3N3b3Jk" http://web-blue:8080/domibus/ext/monitoring/application/status
-curl -v -H "Authorization: Basic cGFkbWluOnBhc3N3b3Jk" http://web-red:8080/domibus/ext/monitoring/application/status
+if [ -f done ]; then
+	exit 0
+fi
+
+curl -v -H "Authorization: Basic cGFkbWluOnBhc3N3b3Jk" -F description=Blue -F file=@domibus-gw-sample-pmode-blue.xml http://web-blue:8080/domibus/ext/pmode
+curl -v -H "Authorization: Basic cGFkbWluOnBhc3N3b3Jk" -F description=Red -F file=@domibus-gw-sample-pmode-red.xml http://web-red:8080/domibus/ext/pmode
+
+touch done
